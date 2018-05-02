ea74ca872480325b25392c675036da293f98ff03
armsarms <armsarms@sina.com>
2018-05-02 16:36:07 +0800

    complete layouts

--- src/layouts/HomeLayout.js
+++ src/layouts/HomeLayout.js
@@ -1,50 +1,76 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Menu, Icon } from 'antd';
-import style from '../styles/home-layout.less';
+import { Link, Router, Route, Switch } from 'react-router-dom';
+import { Layout, Menu, Breadcrumb, Icon, Input } from 'antd';
+import PostList from '../pages/PostList';
+import '../styles/home-layout.css';
+import Home from '../pages/Home';
 
-const SubMenu = Menu.SubMenu;
-const MenuItem = Menu.Item;
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+const Search = Input.Search;
 
 class HomeLayout extends React.Component {
-  render () {
-    const {children} = this.props;
-    return (
-      <div>
-        <header className={style.header}>
-          <Link to="/">ReactManager</Link>
-        </header>
+    render() {
+        const { children } = this.props;
+        return (
+            <div>
+                <Layout className="layout">
+                    <Header className='home-header'>
+                        <div className="logo" />
+                        <Search
+                            placeholder="search something"
+                            onSearch={value => console.log(value)}
+                            style={{ width: 200 }}
+                            enterButton
+                        />
+                        <Menu
+                            theme="dark"
+                            mode="horizontal"
+                            defaultSelectedKeys={['2']}
+                            style={{ lineHeight: '64px', float: 'right' }}
+                        >
+                            <Menu.Item key="1">首页</Menu.Item>
+                            <Menu.Item key="2">登陆</Menu.Item>
+                            <Menu.Item key="3">关于</Menu.Item>
+                        </Menu>
+                    </Header>
+                    <Content style={{ padding: '0 50px' }}>
+                        <Layout style={{ padding: '0 24px 0 0', background: '#fff' }}>
 
-        <main className={style.main}>
-          <div className={style.menu}>
-            <Menu mode="inline" theme="dark" style={{width: '240px'}}>
-              <SubMenu key="user" title={<span><Icon type="user"/><span>用户管理</span></span>}>
-                <MenuItem key="user-list">
-                  <Link to="/user/list">用户列表</Link>
-                </MenuItem>
-                <MenuItem key="user-add">
-                  <Link to="/user/add">添加用户</Link>
-                </MenuItem>
-              </SubMenu>
-
-              <SubMenu key="book" title={<span><Icon type="book"/><span>图书管理</span></span>}>
-                <MenuItem key="book-list">
-                  <Link to="/book/list">图书列表</Link>
-                </MenuItem>
-                <MenuItem key="book-add">
-                  <Link to="/book/add">添加图书</Link>
-                </MenuItem>
-              </SubMenu>
-            </Menu>
-          </div>
-
-          <div className={style.content}>
-            {children}
-          </div>
-        </main>
-      </div>
-    );
-  }
+                            <Content style={{ padding: '0 24px', minHeight: 500 }}>
+                                <Menu mode="horizontal">
+                                    <Menu.Item><Link to='/'>首页</Link></Menu.Item>
+                                    <Menu.Item><Link to='/1'>精华</Link></Menu.Item>
+                                    <Menu.Item><Link to='/2'>分享</Link></Menu.Item>
+                                    <Menu.Item><Link to='/3'>问答</Link></Menu.Item>
+                                </Menu>
+                                <Switch>
+                                    <Route exact path='/' component={Home} />
+                                    <Route path='/1' component={PostList} />
+                                    {/* <Route path='/3/:id' component={Form} /> */}
+                                </Switch>
+                            </Content>
+                            <Sider width={200} style={{ background: '#fff' }}>
+                                <ul className="home-sider">
+                                    <li>1</li>
+                                    <li>2</li>
+                                    <li>3</li>
+                                </ul>
+                            </Sider>
+                        </Layout>
+                    </Content>
+                    <Footer style={{ textAlign: 'center' }}>
+                        <p>Ant Design ©2016 Created by Ant UED</p>
+                    </Footer>
+                </Layout>
+                {/* <main className={style.main}>
+                    <div className={style.content}>
+                        {children}
+                    </div>
+                </main> */}
+            </div>
+        );
+    }
 }
 
 export default HomeLayout;
---
+++ src/pages/PostList.js
@@ -0,0 +1,130 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class PostList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            list: ['']
+        }
+    }
+    componentDidMount() {
+        axios.get('http://localhost:3000/user').then(function (res) {
+            this.setState({ list: res.data })
+            // console.log(this.state.list);
+            // console.log(this.state.list[1]);
+        }.bind(this))
+        // axios({
+        //     method: "get",
+        //     url: 'http://localhost:3000/user',
+        //     headers: {
+        //         'Access-Token': '123456' // 从sessionStorage中获取access token
+        //       }
+        // }).then(function (res) {
+        //     console.log(res);
+        //     this.setState({ list: res.data })
+        // }.bind(this))
+
+        // get('http://localhost:3000/user').then(function (res) {
+        //     if (res) {
+        //         this.setState({ list: res.data })
+        //     }
+        //     // console.log(res);   
+        // }.bind(this)).catch(function (error) {
+        //     console.log(error);
+        //     this.props.history.push('/login')
+        // }.bind(this));
+
+
+        // let token = ''; 
+        // axios({
+        //     method: "post",
+        //     url: 'http://localhost:3000/login',
+        //     data: {
+        //         account: "admin",
+        //         password: "123456"
+        //       }
+        // }).then(function (res) {
+        //     console.log(res.headers['access-token']);
+        //     console.log(res);
+        //     token = res.headers['access-token'];
+        // }.bind(this)).then(function () {
+        //     console.log(token+'haha');
+        //     axios({
+        //         method: "get",
+        //         url: 'http://localhost:3000/user',
+        //         headers: {
+        //             'Access-Token': token // 从sessionStorage中获取access token
+        //           }
+        //     }).then(function (res) {
+        //         console.log(res);
+        //         this.setState({ list: res.data })
+        //     }.bind(this))
+        // }.bind(this))
+
+
+    }
+    // handleEdit(li) {
+    //     // var data = { id: li.id, username: li.username, password: li.password };
+    //     var data = {
+    //         username: {
+    //             valid: true,
+    //             value: li.username,
+    //             error: ''
+    //         },
+    //         password: {
+    //             valid: true,
+    //             value: li.password,
+    //             error: ''
+    //         }
+    //     };
+    //     var path = {
+    //         pathname: '/3/' + li.id,
+    //         state: data,
+    //     }
+    //     this.props.history.push(path)
+    // }
+    // handleDel(li) {
+    //     del('http://localhost:3000/user/' + li.id).then(function (res) {
+    //         this.setState({ list: this.state.list.filter(item => item.id !== li.id) })
+    //         // console.log(res);               
+    //     }.bind(this)).catch(function (error) {
+    //         console.log(error);
+    //     });
+    // }
+
+    render() {
+        const lists = this.state.list;//important
+        return (
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>用户名：</th>
+                            <th>用户密码：</th>
+                            <th>操作</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {lists.map((list) =>
+                            (
+                                <tr key={list.id} className="userList">
+                                    <td>
+                                        {list.username}
+                                    </td>
+                                    <td>
+                                        {list.password}
+                                    </td>
+                                </tr>
+                            )
+                        )}
+                    </tbody>
+                </table>
+
+
+            </div>
+        );
+    }
+}
+
+export default PostList;
