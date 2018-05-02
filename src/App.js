391f71670ea8dff2138b4108314a3a784eff1e85
armsarms <armsarms@sina.com>
2018-05-02 11:23:23 +0800

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import HomeLayout from './layouts/HomeLayout';
+
+class App extends Component {
+  render() {
+    return (
+      <HomeLayout/>
+    );
+  }
+}
+
+export default App;
